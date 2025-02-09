3f98682a36f0976bb8474e6a8fe43d8298bf7053
Michael Fairley <MFairley@users.noreply.github.com>
2025-02-09 16:37:14 -0600

    remove daisyui prior to tailwindcss upgrade

--- website/tailwind.config.cjs
+++ website/tailwind.config.cjs
@@ -4,10 +4,5 @@ module.exports = {
 	theme: {
 		extend: {},
 	},
-	plugins: [require("@tailwindcss/typography"),require("daisyui")],
-	daisyui: {
-		themes: true, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
-		darkTheme: "dark", // name of one of the included themes for dark mode
-		logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
-	  }
+	plugins: [require("@tailwindcss/typography")],
 }
